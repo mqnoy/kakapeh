3c7f71bd17d6be4b1c07a0d283c50ed32886dcf3
mqnoy <mqnoy@github.com>
2020-02-27 06:11:18 +0700

    ubah tampilan

--- src/applaporan/Form_order.java
+++ src/applaporan/Form_order.java
@@ -123,10 +123,7 @@ public class Form_order extends javax.swing.JFrame {
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
